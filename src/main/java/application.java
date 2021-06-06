935ab6b817c35418b1f9a330f5dfe0ac5a0d8122
rahilchughtai <rahilchughtai1@gmail.com>
2021-06-06 17:06:29 +0200

    Using external fxml

---
+++ src/main/java/application.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import java.io.IOException;
+import java.util.Objects;
+
+
+public class application extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("main.fxml")));
+        stage.setTitle("FXML Demo");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+}
