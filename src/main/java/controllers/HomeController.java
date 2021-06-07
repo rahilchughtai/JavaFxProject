badaac3d148ed04938e83cb3db2e074109e1a940
rahilchughtai <rahilchughtai1@gmail.com>
2021-06-07 16:40:11 +0200

    Testing out switching scene/stage
    TODO: Dont make it open new window

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -11,6 +12,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
@@ -28,12 +30,18 @@ public class HomeController implements Initializable {
         //Add event listeners for the buttons
     }
 
+
+    private void loadStage(String fxml, Event eventSource) {
+
+
+    }
     private void loadStage(String fxml) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource(fxml));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource(fxml)));
             Stage stage = new Stage();
             stage.setScene(new Scene(root));
             stage.show();
+
         } catch (IOException e) {
             System.out.println("Something went wrong with loading the fxml!");
         }
@@ -45,9 +53,12 @@ public class HomeController implements Initializable {
 
         if(clickSource==btnCourses){
             System.out.println("Courses!");
+            loadStage("views/Courses.fxml");
         }
         else if (clickSource==btnStudents){
             System.out.println("Students!!!");
+            loadStage("views/Students.fxml");
+
         }
         /*
         if (mouseEvent.getSource() == btnDashboard) {
