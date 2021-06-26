511b4cf425925a43bc99e4485214361d15e33e70
Tauboss777 <danieltaube383@gmail.com>
2021-06-26 22:01:48 +0200

    Courses Table with edit and update function

--- src/main/java/controllers/CoursesController.java
+++ src/main/java/controllers/CoursesController.java
@@ -1,5 +1,95 @@
 package controllers;
 
-public class CoursesController extends SceneController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import model.Course;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CoursesController extends SceneController implements Initializable  {
+
+    @FXML
+    private TableView<Course> table_info;
+
+    public static TableView<Course> table_info_2;
+
+    @FXML
+    private TableColumn<Course, String> col_cid;
+
+    @FXML
+    private TableColumn<Course, String> col_courseName;
+
+    @FXML
+    private TableColumn<Course, String> col_room;
+
+    @FXML
+    private TableColumn<Course, Button> col_update;
+
+    public static ObservableList<Course> data_table;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        table_info_2 = table_info;
+        initTable();
+        loadData();
+    }
+
+    private void initTable() {
+        initCols();
+    }
+
+    private void initCols() {
+
+        col_cid.setCellValueFactory(new PropertyValueFactory<>("cid"));
+        col_courseName.setCellValueFactory(new PropertyValueFactory<>("courseName"));
+        col_room.setCellValueFactory(new PropertyValueFactory<>("room"));
+        col_update.setCellValueFactory(new PropertyValueFactory<>("update"));
+
+        editableCols();
+    }
+
+    private void editableCols() {
+        col_cid.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        col_cid.setOnEditCommit(e->{
+                    e.getTableView().getItems().get(e.getTablePosition().getRow()).setCid(e.getNewValue());
+        });
+
+
+        col_courseName.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        col_courseName.setOnEditCommit(e->{
+            e.getTableView().getItems().get(e.getTablePosition().getRow()).setCourse(e.getNewValue());
+        });
+
+
+        col_room.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        col_room.setOnEditCommit(e->{
+            e.getTableView().getItems().get(e.getTablePosition().getRow()).setRoom(e.getNewValue());
+        });
+
+        table_info.setEditable(true);
+
+    }
+
+    private void loadData() {
+        data_table = FXCollections.observableArrayList();
+
+        for (int i = 0; i < 10; i++) {
+            data_table.add(new Course(String.valueOf(i),
+                    "courseName " + i, "room " + i, new Button("update")));
+        }
+
+        table_info.setItems(data_table);
+
+    }
 }
--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -1,4 +1,66 @@
 package model;
 
+
+import controllers.CoursesController;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+
 public class Course {
+    String cid, courseName, room;
+
+    Button update;
+
+    public Course(String cid, String courseName, String room, Button update) {
+        this.cid = cid;
+        this.courseName = courseName;
+        this.room = room;
+        this.update = update;
+
+        update.setOnAction(e ->{
+
+            ObservableList<Course> courses = CoursesController.table_info_2.getSelectionModel().getSelectedItems();
+
+            for (Course course: courses) {
+                if (course.getUpdate() == update) {
+                    System.out.println("id: "+ course.getCid());
+                    System.out.println("course: "+ course.getCourseName());
+                    System.out.println("room: "+ course.getRoom());
+                }
+
+            }
+        });
+
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourse(String course) {
+        this.courseName = course;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public Button getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(Button update) {
+        this.update = update;
+    }
 }
