716d1e8a46dffd9ca5c3daa07f6865127a1656ad
Michel <michel.goerlach2602@gmail.com>
2021-06-28 22:42:38 +0200

    Fixed small bug StudentsController needs refactoring

--- src/main/java/controllers/StudentsController.java
+++ src/main/java/controllers/StudentsController.java
@@ -1,5 +1,27 @@
 package controllers;
 
+import com.sun.javafx.scene.control.IntegerField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class StudentsController extends SceneController{
 
+    @FXML private TextField matrikelNumberField;
+    @FXML private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private TextField companyField;
+    @FXML private TextField javaSkillsField;
+
+    private int matrikelNumber;
+    private String firstName;
+    private String lastName;
+    private String companyName;
+
+
+    @FXML
+    private void addNewStudent(ActionEvent actionEvent){
+
+    }
 }
