e7dd746674b57322ac189fbc13d8d12d76fbbae0
rahilchughtai <rahilchughtai1@gmail.com>
2021-06-29 13:19:39 +0200

    some testing for add student

--- src/main/java/controllers/StudentsController.java
+++ src/main/java/controllers/StudentsController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,20 +12,25 @@ public class StudentsController extends SceneController {
 
     @FXML
     private TextField matrikelNumberField;
+
     @FXML
     private TextField firstNameField;
+
     @FXML
     private TextField lastNameField;
+
     @FXML
     private TextField companyField;
-    @FXML
-    private TextField javaSkillsField;
 
+    @FXML
+    private ComboBox<String> javaSkillsComboBox;
+
+    /*
     private int matrikelNumber;
     private String firstName;
     private String lastName;
     private String companyName;
-
+*/
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -34,6 +40,12 @@ public class StudentsController extends SceneController {
 
     @FXML
     private void addNewStudent(ActionEvent actionEvent) {
+        // Refactor this and implement addNewStudentMethod, use Student Model
+        System.out.println(firstNameField.getText());
+        System.out.println(lastNameField.getText());
+        System.out.println(javaSkillsComboBox.getValue());
+
+        System.out.println("Adding a new student!!");
 
     }
 }
