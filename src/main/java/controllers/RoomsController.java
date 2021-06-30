ec038e3ce1787bfb6a095e10786256f6055a5fbd
rahilchughtai <rahilchughtai1@gmail.com>
2021-07-01 00:19:40 +0200

    add new room

--- src/main/java/controllers/RoomsController.java
+++ src/main/java/controllers/RoomsController.java
@@ -1,52 +1,46 @@
 package controllers;
 
-import database.models.JavaSkillRating;
-import database.models.Student;
-import database.services.DummyDataService;
 import database.services.ModelService;
 import database.services.RoomService;
-import database.services.StudentService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import models.Room;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class RoomsController extends SceneController {
 
+    public static ObservableList<Room> data_table;
+    public TextField text_roomName;
     private ModelService<database.models.Room> roomService;
-
-    @FXML
-    private TableView<Room> table_rooms;
+    private RoomService roomDbService;
 
     //@FXML
     //private TableColumn<Room, String> columnRoomName;
-
-    public static ObservableList<Room> data_table;
+    @FXML
+    private TableView<Room> table_rooms;
 
     @FXML
-    private void addNewRoom(ActionEvent actionEvent) {
-
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){
-        roomService = RoomService.getService();
-
+    private void addNewRoom(ActionEvent actionEvent) throws SQLException {
+        roomService.save(new ArrayList<database.models.Room>() {{ add(new database.models.Room(null, text_roomName.getText())); }});
         loadData();
     }
 
-    private void loadData(){
-        data_table = FXCollections.observableArrayList();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        roomService = RoomService.getService();
+        loadData();
+    }
 
+    private void loadData() {
+        data_table = FXCollections.observableArrayList();
         try {
             final var rooms = roomService
                     .get()
@@ -61,7 +55,7 @@ public class RoomsController extends SceneController {
     }
 
     @FXML
-    private void saveRooms(ActionEvent actionEvent){
+    private void saveRooms(ActionEvent actionEvent) {
 
     }
 }
