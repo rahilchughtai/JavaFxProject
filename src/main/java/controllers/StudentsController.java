293d30ded365334c53bf14d498598e86533788b4
rahilchughtai <rahilchughtai1@gmail.com>
2021-06-30 22:36:19 +0200

    preparing dummy data load for StudentsController

--- src/main/java/controllers/StudentsController.java
+++ src/main/java/controllers/StudentsController.java
@@ -1,13 +1,16 @@
 package controllers;
 
 import database.models.JavaSkillRating;
-import database.models.Student;
 import database.services.ModelService;
 import database.services.StudentService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import models.Student;
 
 import java.net.URL;
 import java.sql.SQLException;
@@ -18,30 +21,25 @@ import java.util.ResourceBundle;
 
 public class StudentsController extends SceneController {
 
-    private ModelService<Student> studentService;
-
+    public static ObservableList<Student> data_table;
+    public TableView<Student> table_Students;
+    private ModelService<database.models.Student> studentService;
     @FXML
     private TextField text_newMatrikelNumber;
-
     @FXML
     private TextField text_newFirstName;
-
     @FXML
     private TextField text_newLastName;
-
     @FXML
     private TextField text_newCorporation;
-
     @FXML
     private ComboBox<String> combo_javaSkills;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
         studentService = StudentService.getService();
-
         textFieldToNumberField(text_newMatrikelNumber);
+        loadData();
 
     }
 
@@ -51,15 +49,36 @@ public class StudentsController extends SceneController {
         return JavaSkillRating.valueOf(skillRating.toUpperCase());
     }
 
+    private void loadData() {
+        data_table = FXCollections.observableArrayList();
+        try {
+            data_table.addAll(
+                    studentService.get().stream().map(
+                            s -> new Student(
+                                    s.getMatriculationNumber(),
+                                    s.getFirstName(),
+                                    s.getLastName(),
+                                    s.getCorporationName(),
+                                    s.getJavaSkillRating()
+                            )
+                    ).toList()
+            );
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+        table_Students.setItems(data_table);
+    }
+
     @FXML
     private void addNewStudent(ActionEvent actionEvent) throws SQLException {
-        Collection<Student> students;
-        studentService.save(new ArrayList<Student>() {
+        Collection<database.models.Student> students;
+        studentService.save(new ArrayList<database.models.Student>() {
             {
                 add(
-                        new Student() {
+                        new database.models.Student() {
                             {
-
                                 setMatriculationNumber(text_newMatrikelNumber.getText());
                                 setFirstName(text_newFirstName.getText());
                                 setLastName(text_newLastName.getText());
