a2f058c538f53be9aad33477e1b475e6c75bbaac
David Hedtke <d.hedtke@protonmail.com>
2021-06-30 23:03:25 +0200

    Adding course is now working

--- src/main/java/controllers/CoursesController.java
+++ src/main/java/controllers/CoursesController.java
@@ -3,6 +3,7 @@ package controllers;
 import database.models.Room;
 import database.services.CourseService;
 import database.services.ModelService;
+import database.services.RoomService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,14 +13,16 @@ import models.Course;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.ResourceBundle;
 
 public class CoursesController extends SceneController {
 
     private ModelService<database.models.Course> courseService;
+    private ModelService<database.models.Room> roomService;
 
-    private Collection<Room> possibleRooms;
+    private Collection<database.models.Room> possibleRooms;
 
     @FXML
     private ComboBox<String> combo_room;
@@ -31,28 +34,52 @@ public class CoursesController extends SceneController {
     private TextField text_newCourseName;
 
     @FXML
-    private TextField text_newRoomName;
-
-    @FXML
     private void addNewCourse(ActionEvent actionEvent) {
-//        final var newCourse = new Course(text_newCourseName.getText(), text_newRoomName.getText());
+        final var selectedRoomName = combo_room.getSelectionModel().getSelectedItem();
+        final var newCourseName = text_newCourseName.getText();
+
+        if (selectedRoomName == null || newCourseName.equals(""))
+            return;
+
+        final var selectedRoom = possibleRooms.stream().filter(x -> x.getName().equals(selectedRoomName)).findFirst().get();
+        final var newCourse = new Course(newCourseName, selectedRoom.getId(), selectedRoom.getName());
+
+        try {
+            courseService.save(new ArrayList<>() {{ add(new database.models.Course(null, newCourse.getName(), new Room(newCourse.getRoomId(), newCourse.getRoomName()))); }});
+
+            data_courses.add(newCourse);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
-    public static ObservableList<Course> data_table;
+    public static ObservableList<Course> data_courses;
+    public static ObservableList<String> data_roomNames;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         courseService = CourseService.getService();
+        roomService = RoomService.getService();
 
         loadData();
     }
 
     private void loadData() {
-        data_table = FXCollections.observableArrayList();
+        data_courses = FXCollections.observableArrayList();
+        data_roomNames = FXCollections.observableArrayList();
 
         try {
-            data_table.addAll(courseService
+            possibleRooms = roomService.get();
+
+            data_roomNames.addAll(possibleRooms
+                .stream()
+                .map(x -> x.getName())
+                .toList());
+
+            combo_room.setItems(data_roomNames);
+
+            data_courses.addAll(courseService
                     .get()
                     .stream()
                     .map(x -> new Course(x.getName(), x.getRoom().getId(), x.getRoom().getName()))
@@ -62,6 +89,6 @@ public class CoursesController extends SceneController {
             sqlException.printStackTrace();
         }
 
-        table_info.setItems(data_table);
+        table_info.setItems(data_courses);
     }
 }
