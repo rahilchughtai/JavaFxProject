07455dadb489bae2263dd1a59128c504b586a0f8
Michel <michel.goerlach2602@gmail.com>
2021-06-30 22:21:08 +0200

    Rooms Controller

---
+++ src/main/java/models/Room.java
@@ -0,0 +1,56 @@
+package models;
+
+import database.models.Model;
+
+import java.util.Objects;
+
+public class Room implements Model {
+
+    private Integer id;
+    private String name;
+
+    public Room() {
+    }
+
+    public Room(final Integer id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return Objects.equals(id, room.id) && Objects.equals(name, room.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
